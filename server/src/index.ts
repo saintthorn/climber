18b16285fe5641958406056b7e1377661737c48f
Spencer Thornhill <spencerthornhill@icloud.com>
2025-12-03 17:45:48 -0800

    init html

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,22 @@
 import express, { type Request, type Response } from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PUBLIC_PATH = path.join(__dirname, '../../client/public');
 
 const PORT: number = 3000;
 const app = express();
 
+app.use(express.static(PUBLIC_PATH));
+
 app.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'Hello from climber!'});
+    res.sendFile(path.join(PUBLIC_PATH, 'index.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
+
 
